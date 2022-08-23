99decfaba966419f65435ec4c3f424938ec3a74b
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-08-23 11:16:49 +0100

    Add different information based on what the user wants (current, forecast)

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import Country from '../Country';
 import Weather from '../Weather';
 import style from './style.module.css';
 
-const Info = () => (
-  <div className={style.card}>
-    <ul className={style.nav}>
-      <li>CURRENT</li>
-      <li>FORECAST</li>
-    </ul>
-    <div className={style.info}>
-      <Country />
-      <Weather />
+const Info = () => {
+  const [forecast, setForecast] = useState(false);
+
+  const handleClick = (e) => {
+    if (e.target.innerText === 'CURRENT') {
+      setForecast(false);
+    } else setForecast(true);
+  };
+
+  return (
+    <div className={style.card}>
+      <ul className={style.nav}>
+        <button
+          onClick={(e) => handleClick(e)}
+          type="button"
+        >
+          CURRENT
+        </button>
+        <button
+          onClick={(e) => handleClick(e)}
+          type="button"
+        >
+          FORECAST
+        </button>
+      </ul>
+      <div className={style.info}>
+        {!forecast ? (
+          <>
+            <Country />
+            <Weather />
+          </>
+        ) : <div><h1>TEMPLATE TEXT</h1></div>}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Info;
