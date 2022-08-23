a7dc92d250363ca4ec17bf57248c4900107e9839
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-08-23 06:51:33 +0100

    Dispatch the getLocation function every time a user loads the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import Weather from './components/Weather';
+import { getLocation } from './app/weather';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Get the lat and lon coords
+    navigator.geolocation.getCurrentPosition((p) => {
+      // Get the weather information with the lat and lon provided
+      dispatch(getLocation(`${p.coords.latitude},${p.coords.longitude}`));
+    }, () => {
+      // Else get the weather information with the IP location
+      dispatch(getLocation('auto:ip'));
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
