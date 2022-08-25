7ca865fb1f0cbb5c03f08f23a95e108f9fad94eb
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-08-25 12:29:34 +0100

    Forecast now shows data from the weather API

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -1,36 +1,50 @@
+import PropTypes from 'prop-types';
 import style from './style.module.css';
 
-const Day = () => (
-  <div className={style.day}>
-    <h2>DAY 1</h2>
-    <div className={style.weather}>
-      <div className={style.card}>
-        <h3>20ºC</h3>
-        <img src="https://cdn.weatherapi.com/weather/64x64/night/353.png" alt="Patchy" />
-        <h3>12 AM</h3>
-      </div>
-      <div className={style.card}>
-        <h3>20ºC</h3>
-        <img src="https://cdn.weatherapi.com/weather/64x64/night/353.png" alt="Patchy" />
-        <h3>6 AM</h3>
-      </div>
-      <div className={style.card}>
-        <h3>20ºC</h3>
-        <img src="https://cdn.weatherapi.com/weather/64x64/night/353.png" alt="Patchy" />
-        <h3>NOON</h3>
-      </div>
-      <div className={style.card}>
-        <h3>20ºC</h3>
-        <img src="https://cdn.weatherapi.com/weather/64x64/night/353.png" alt="Patchy" />
-        <h3>6 PM</h3>
-      </div>
-      <div className={style.card}>
-        <h3>20ºC</h3>
-        <img src="https://cdn.weatherapi.com/weather/64x64/night/353.png" alt="Patchy" />
-        <h3>12 AM</h3>
+const Day = ({ day }) => {
+  const times = [0, 6, 12, 18, 23];
+
+  const toTwelveHour = (hour) => {
+    // The hour will be a string with this format "hour:minutes"
+    const betterHour = Number(hour);
+
+    switch (true) {
+      case (betterHour === 0):
+      case (betterHour === 24):
+        return '12 AM';
+
+      case (betterHour < 12):
+        return `${betterHour} AM`;
+
+      case (betterHour === 12):
+        return '12 PM';
+
+      case (betterHour >= 12):
+        return `${betterHour - 12} PM`;
+
+      default:
+        return hour;
+    }
+  };
+
+  return (
+    <div className={style.day}>
+      <h2>{day.date}</h2>
+      <div className={style.weather}>
+        {times.map((time) => (
+          <div key={time} className={style.card}>
+            <h3>{day.hour[time].temp_c}</h3>
+            <img src={day.hour[time].condition.icon} alt={day.hour[time].condition.text} />
+            <h3>{time !== 23 ? toTwelveHour(day.hour[time].time.slice(11, 13)) : '12 AM'}</h3>
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+Day.propTypes = {
+  day: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default Day;
--- src/components/Forecast/index.js
+++ src/components/Forecast/index.js
@@ -1,16 +1,21 @@
+import { useSelector } from 'react-redux';
 import Day from '../Day';
 import style from './style.module.css';
 
-const Forecast = () => (
-  <div className={style.info}>
-    <div className={style.country}>
-      <h1>LAGOS</h1>
-      <small>PORTUGAL</small>
+const Forecast = () => {
+  const { forecast } = useSelector((state) => state.weather.location);
+
+  return (
+    <div className={style.info}>
+      <div className={style.country}>
+        <h1>LAGOS</h1>
+        <small>PORTUGAL</small>
+      </div>
+      <div className={style.forecast}>
+        {forecast.forecastday.map((day) => <Day key={day.date_epoch} day={day} />)}
+      </div>
     </div>
-    <div className={style.forecast}>
-      <Day />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Forecast;
