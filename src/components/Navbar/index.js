f0645691e709aa91d85bfe3ca4bbbb0ae7ea2cc6
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-09-03 15:41:27 +0100

    Center Search Component in desktop version

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -105,7 +105,7 @@ const Navbar = () => {
         </div>
         )}
 
-        <div style={{ marginBottom: 'auto' }}>
+        <div className={style.search}>
           <Search />
         </div>
       </nav>
